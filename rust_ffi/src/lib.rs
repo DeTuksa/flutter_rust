13000d8337c52c4e89ad9af8ad98672229b8ab02
DeTuksa <emmydave414@gmail.com>
2024-08-16 12:14:28 +0100

    simple weather fetch

--- rust_ffi/src/lib.rs
+++ rust_ffi/src/lib.rs
@@ -1,14 +1,19 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use serde::{Deserialize, Serialize};
+use std::ffi::{CString, CStr};
+
+#[derive(Deserialize, Debug, Serialize)]
+pub struct Weather {
+    pub temperature: f64,
+    pub description: String,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[no_mangle]
+pub extern "C" fn fetch_weather(api_url: *const i8) -> *mut i8 {
+    let api_url = unsafe { CStr::from_ptr(api_url).to_str().unwrap() };
+    let weather: Weather = reqwest::blocking::get(api_url)
+        .unwrap()
+        .json()
+        .unwrap();
+    let weather_json = serde_json::to_string(&weather).unwrap();
+    CString::new(weather_json).unwrap().into_raw()
 }
